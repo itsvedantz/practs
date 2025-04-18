7092cc87b3a41fa178c448331f855094eee24056
itsvedant <97307771+itsvedantz@users.noreply.github.com>
2025-04-18 21:14:19 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -4,7 +4,7 @@ const cyberGrid = document.querySelector('.cyber-grid');
 const pdfFiles = [
     { name: 'BIDS', path: 'bids.pdf' },
     { name: 'SIS', path: 'AMP_PracticalFinal.pdf' },
-    { name: 'GIS', path: 'path/to/file3.pdf' },
+    { name: 'GIS', path: 'gis.pdf' },
     { name: 'Android programming', path: 'AMP_PracticalFinal.pdf' },
 
     // Add more file paths as needed
