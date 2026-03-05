38ab646e4d7907185cb5129ba7cbb8c0d95eddfc
Vedant Mohite <imvedant22@gmail.com>
2026-03-06 00:45:27 +0530

    Revise PDF file paths and names in scripts.js
    Updated the PDF file paths and names in the cyberGrid.

--- scripts.js
+++ scripts.js
@@ -2,9 +2,23 @@ const cyberGrid = document.querySelector('.cyber-grid');
 
 // Assuming you have an array of PDF file paths
 const pdfFiles = [
-    { name: 'DS', path: 'desisiontree.txt' }, 
+    { name: 'Logistic Regression Pivot Table', path: 'Logistic Regression-pivottable.txt' },
+    { name: 'Read CSV and Perform Data Pre Processing', path: 'Read CSV and Perform Data Pre-Pr.txt' },
+    { name: 'Linear Regression VLOOKUP', path: 'Linear RegressionVLOOKUP.txt' },
+    { name: 'Linear Regression Goal Seek', path: 'Linear Regressionoalseek.txt' },
+    { name: 'Restaurant Waiting Decision Tree', path: 'Restaurant Waiting Decision Tree.txt' },
 
-
+    { name: 'Decision Tree on Titanic Dataset', path: 'Decision Tree on Titanic Dataset.txt' },
+    { name: 'Decision Tree Tennis Wind', path: 'Decision Treettennieswind.txt' },
+    { name: 'One Way ANOVA PCA on Wine Quality', path: 'One-WayANOVAPCAonWinequality.txt' },
+    { name: 'Read CSV File into Data Frame', path: 'Read CSV File into Data Frame.txt' },
+    { name: 'Feature Dumification Logistic Regression', path: 'Feature Dumification logisticreg.txt' },
+    { name: 'Feature Scaling PCA', path: 'Feature ScalingPCA.txt' },
+    { name: 'Python Code Performs Logistic Regression', path: 'Python code performs Logistic Re.txt' },
+    { name: 'Paired T-Test', path: 'Paired T-Test.txt' },
+    { name: 'Chi Square Test Logistic', path: 'Chi Square Test-logistic.txt' },
+    { name: 'One Sample T-Test Feature Scaling', path: 'One Sample T-Test-featurescaling.txt' },
+    { name: 'K-Means Clustering + Elbow Method', path: 'K-Means Clustering + Elbow Metho.txt' } 
 
     // Add more file paths as needed
 ];
@@ -27,6 +41,7 @@ pdfFiles.forEach((file) => {
 
     cyberGrid.appendChild(cyberCard);
 });
+
 
 
 
