5983c78e9e5ce1b1c5a5e9e197e94f273e61fe34
Vedant Mohite <imvedant22@gmail.com>
2026-03-09 00:53:30 +0530

    Update script.js

--- eh/script.js
+++ eh/script.js
@@ -3,23 +3,23 @@ const cyberGrid = document.querySelector('.cyber-grid');
 // Assuming you have an array of PDF file paths
 const pdfFiles = [
     { name: 'inverted index, question-answering system ', path: 'Slip_1.txt' },
-    { name: 'Slip 2', path: 'Slip_2.txt' },
-    { name: 'Slip 3', path: 'Slip_3.txt' },
-    { name: 'Slip 4', path: 'Slip_4.txt' },
-    { name: 'Slip 5', path: 'Slip_5.txt' },
-    { name: 'Slip 6', path: 'Slip_6.txt' },
-    { name: 'Slip 7', path: 'Slip_7.txt' },
-    { name: 'Slip 8', path: 'Slip_8.txt' },
-    { name: 'Slip 9', path: 'Slip_9.txt' },
-    { name: 'Slip 10', path: 'Slip_10.txt' },
-    { name: 'Slip 11', path: 'Slip_11.txt' },
-    { name: 'Slip 12', path: 'Slip_12.txt' },
-    { name: 'Slip 13', path: 'Slip_13.txt' },
-    { name: 'Slip 14', path: 'Slip_14.txt' },
-    { name: 'Slip 15', path: 'Slip_15.txt' },
-    { name: 'Slip 16', path: 'Slip_16.txt' },
-    { name: 'Slip 17', path: 'Slip_17.txt' },
-    { name: 'Slip 18', path: 'Slip_18.txt' },
+    { name: 'inverted index 2doc, calculate precision', path: 'Slip_2.txt' },
+    { name: 'spelling correction, Boolean retrieval', path: 'Slip_3.txt' },
+    { name: 'web crawler, Handle challenges such as robots.txt', path: 'Slip_4.txt' },
+    { name: 'simplified web graph, text summarization', path: 'Slip_5.txt' },
+    { name: 'cosine similarity, question-answering system', path: 'Slip_6.txt' },
+    { name: 'text summarization,  Boolean retrieval model', path: 'Slip_7.txt' },
+    { name: 'Vector Space Model, Cosine Similarity', path: 'Slip_8.txt' },
+    { name: 'Calculate Precision, Evaluation Toolkit', path: 'Slip_9.txt' },
+    { name: 'Naïve Bayes, Classification using SVM', path: 'Slip_10.txt' },
+    { name: 'Question Answering System, Calculate Precision', path: 'Slip_11.txt' },
+    { name: 'PageRank Algorithm, Calculate Precision', path: 'Slip_12.txt' },
+    { name: 'Vector Space Model, Clustering Algorithm', path: 'Slip_13.txt' },
+    { name: 'Clustering Algorithm, Binary Classification', path: 'Slip_14.txt' },
+    { name: 'Inverted Index Construction, Simple Document Retrieval', path: 'Slip_15.txt' },
+    { name: 'Vector Space Model with TF-IDF, Clustering Algorithm', path: 'Slip_16.txt' },
+    { name: 'PageRank Algorithm, Boolean Retrieval Model', path: 'Slip_17.txt' },
+    { name: 'Question Answering System, Text Summarization', path: 'Slip_18.txt' },
     { name: 'Slip 19', path: 'Slip_19.txt' },
     { name: 'Slip 20', path: 'Slip_20.txt' }
     // Add more file paths as needed
@@ -43,6 +43,7 @@ pdfFiles.forEach((file) => {
 
     cyberGrid.appendChild(cyberCard);
 });
+
 
 
 
