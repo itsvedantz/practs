141a22a407ad079d88b5269fb88124f5a68ea29f
Vedant Mohite <imvedant22@gmail.com>
2026-03-31 20:52:04 +0530

    Update script.js

--- CN/script.js
+++ CN/script.js
@@ -2,63 +2,52 @@ const cyberGrid = document.querySelector('.cyber-grid');
 
 const files = [
 
-    {
-        name: "Creating SOAP Web Service using Java",
-        type: "youtube",
-        id: "hM1xBlNaZv0"
-    },
-
-    {
-        name: "Creating and Consuming Java Web Service",
-        type: "youtube",
-        id: "JL5clLdulrE"
-    },
-
-    {
-        name: "Creating Web Service in Java and Consuming in .Net",
-        type: "youtube",
-        id: "fjpCtcJ9oog"
-    },
-
+  {
+    name: "P1 Execute following networking commands....SystemInfo",
+    type: "file",
+    path: "P1 Execute following networking commands....SystemInfo.pkt",
+  },
+  {
+    name: "P2 Create a basic network of two computers",
+    type: "file",
+    path: "P2 Create a basic network of two computers.pkt",
+  },
+  {
+    name: "P3 Create a basic network of one server & two computers",
+    type: "file",
+    path: "P3 Create a basic network of one server & two computers.pkt",
+  },
+  {
+    name: "P4 Create a basic network of one server & two computers & two mobile devices",
+    type: "file",
+    path: "P4 Create a basic network of one server & two computers & two mobile devices.pkt",
+  },
+  {
+    name: "P5 Create a network with three routes with RIPv1",
+    type: "file",
+    path: "P5 Create a network with three routes with RIPv1.pkt",
+  },
+  {
+    name: "P6 Create a network with three routers with RIPv2",
+    type: "file",
+    path: "P6 Create a network with three routers with RIPv2.pkt",
+  },
+  {
+    name: "P7 Create a network with three routers with OSPF",
+    type: "file",
+    path: "P7 Create a network with three routers with OSPF.pkt",
+  },
+  {
+    name: "P8 Create a network with three routers with BGP",
+    type: "file",
+    path: "P8 Create a network with three routers with BGP.pkt",
+  },
+  {
+    name: "P9 Create a wireless network of multiple PCs",
+    type: "file",
+    path: "P9 Create a wireless network of multiple PCs.pkt",
+  }
         
-    {
-        name: "CRUD Operations",
-        type: "youtube",
-        id: "l-Ry3BxiNh4"
-    },
-
-       
-    {
-        name: "Download Image From Server Using MTOM",
-        type: "youtube",
-        id: "FhOLswDbfSQ"
-    },
-
-            
-    {
-        name: "google map",
-        type: "youtube",
-        id: "01krzNSRK-o"
-    },
-
-    {
-        name: "MTOM",
-        type: "file",
-        path: "mtom.pdf",
-    },
-
-        {
-        name: "Soap 10 slips",
-        type: "file",
-        path: "soapslips.pdf"
-    },
-    
-    {
-        name: "Curd",
-        type: "file",
-        path: "Curd.txt"
-    }
-
 ];
 
 files.forEach((file) => {
