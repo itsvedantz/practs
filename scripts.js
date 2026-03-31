dc7850cd2a3f4a10a41d327c9197d21464e20d8d
Vedant Mohite <imvedant22@gmail.com>
2026-03-31 21:09:40 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,101 +1,63 @@
 const cyberGrid = document.querySelector('.cyber-grid');
 
-const files = [
+// Assuming you have an array of PDF file paths
+const pdfFiles = [
+    { name: 'CN', path: 'practs/cn' },
+    { name: 'inverted index 2doc, calculate precision', path: 'Slip_2.txt' },
 
-    {
-        name: "Creating SOAP Web Service using Java",
-        type: "youtube",
-        id: "hM1xBlNaZv0"
-    },
-
-    {
-        name: "Creating and Consuming Java Web Service",
-        type: "youtube",
-        id: "JL5clLdulrE"
-    },
-
-    {
-        name: "Creating Web Service in Java and Consuming in .Net",
-        type: "youtube",
-        id: "fjpCtcJ9oog"
-    },
-
-        
-    {
-        name: "CRUD Operations",
-        type: "youtube",
-        id: "l-Ry3BxiNh4"
-    },
-
-       
-    {
-        name: "Download Image From Server Using MTOM",
-        type: "youtube",
-        id: "FhOLswDbfSQ"
-    },
-
-            
-    {
-        name: "google map",
-        type: "youtube",
-        id: "01krzNSRK-o"
-    },
-
-    {
-        name: "MTOM",
-        type: "file",
-        path: "mtom.pdf",
-    },
-
-        {
-        name: "Soap 10 slips",
-        type: "file",
-        path: "soapslips.pdf"
-    },
-    
-    {
-        name: "Curd",
-        type: "file",
-        path: "Curd.txt"
-    }
-
+    // Add more file paths as needed
 ];
 
-files.forEach((file) => {
-
+pdfFiles.forEach((file) => {
     const cyberCard = document.createElement('div');
     cyberCard.classList.add('cyber-card');
 
-    const title = document.createElement('h3');
-    title.innerText = file.name;
+    const link = document.createElement('a');
+    link.href = file.path;
+    link.textContent = file.name;
+    link.classList.add('file-name');
 
-    cyberCard.appendChild(title);
+    const fileSize = document.createElement('p');
+    fileSize.classList.add('file-size');
+    fileSize.textContent = file.size;
 
-    if (file.type === "youtube") {
-
-        const iframe = document.createElement('iframe');
-
-        iframe.src = `https://www.youtube.com/embed/${file.id}`;
-        iframe.width = "100%";
-        iframe.height = "200";
-        iframe.allowFullscreen = true;
-        iframe.frameBorder = "0";
-
-        cyberCard.appendChild(iframe);
-
-    }
-
-    if (file.type === "file") {
-
-        const link = document.createElement('a');
-        link.href = file.path;
-        link.innerText = "Open File";
-
-        cyberCard.appendChild(link);
-    }
+    cyberCard.appendChild(link);
+    cyberCard.appendChild(fileSize);
 
     cyberGrid.appendChild(cyberCard);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
