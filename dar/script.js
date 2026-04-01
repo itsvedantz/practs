5b80ce1ccbc73da428077152c495b3b1e5f97dcd
Vedant Mohite <imvedant22@gmail.com>
2026-04-01 22:43:03 +0530

    Add script.js to create dynamic file links

---
+++ dar/script.js
@@ -0,0 +1,34 @@
+const cyberGrid = document.querySelector('.cyber-grid');
+
+// Assuming you have an array of PDF file paths
+const pdfFiles = [
+    { name: 'P1 Execute following networking commands....SystemInfo', path: 'P1 Execute following networking commands....SystemInfo.pkt' },
+    { name: 'ins code', path: 'ins.txt' },
+    { name: 'pract6', path: 'pract6.pkt' },
+    { name: 'pract7', path: 'pract7.pkt' },
+    { name: 'pract8', path: 'pract8.pkt' },
+    { name: 'pr6', path: 'pr6.png' },
+    { name: 'pr7', path: 'pr7.png' },
+    { name: 'pr8', path: 'pr8.png' },
+
+    // Add more file paths as needed
+];
+
+pdfFiles.forEach((file) => {
+    const cyberCard = document.createElement('div');
+    cyberCard.classList.add('cyber-card');
+
+    const link = document.createElement('a');
+    link.href = file.path;
+    link.textContent = file.name;
+    link.classList.add('file-name');
+
+    const fileSize = document.createElement('p');
+    fileSize.classList.add('file-size');
+    fileSize.textContent = file.size;
+
+    cyberCard.appendChild(link);
+    cyberCard.appendChild(fileSize);
+
+    cyberGrid.appendChild(cyberCard);
+});
