9d064f891425614476e1dea4a1ace13561002c4a
Vedant Mohite <imvedant22@gmail.com>
2026-04-04 11:16:07 +0530

    Update script.js

--- dar/script.js
+++ dar/script.js
@@ -4,16 +4,16 @@ const cyberGrid = document.querySelector('.cyber-grid');
 const pdfFiles = [
 
   { name: 'BOOK', path: 'BOOK.CSV' },
-  { name: 'M1P11 Reshaping data', path: 'M1P11 Reshaping data.R' },
-  { name: 'M1P2 Creating datasets from raw data', path: 'M1P2 Creating datasets from raw data.R' },
-  { name: 'M1P3 Exploring data- Displaying datasets', path: 'M1P3 Exploring data- Displaying datasets.R' },
-  { name: 'M1P5 Sorting data', path: 'M1P5 Sorting data.R' },
-  { name: 'M1P7 Selecting & dropping variables', path: 'M1P7 Selecting & dropping variables.R' },
-  { name: 'M1P9 Performing text manipulation', path: 'M1P9 Performing text manipulation.R' },
-  { name: 'M2P1 generating frequency tables', path: 'M2P1 generating frequency tables.R' },
-  { name: 'M2P3 performing one-sample + -tests', path: 'M2P3 performing one-sample + -tests.R' },
-  { name: 'M2P5 performing paired t tests', path: 'M2P5 performing paired t tests.R' },
-  { name: 'M2P6 performing one-way ANOVA', path: 'M2P6 performing one-way ANOVA.R' }
+  { name: 'M1P11 Reshaping data', path: 'M1P11 Reshaping data.txt' },
+  { name: 'M1P2 Creating datasets from raw data', path: 'M1P2 Creating datasets from raw data.txt' },
+  { name: 'M1P3 Exploring data- Displaying datasets', path: 'M1P3 Exploring data- Displaying datasets.txt' },
+  { name: 'M1P5 Sorting data', path: 'M1P5 Sorting data.txt' },
+  { name: 'M1P7 Selecting & dropping variables', path: 'M1P7 Selecting & dropping variables.txt' },
+  { name: 'M1P9 Performing text manipulation', path: 'M1P9 Performing text manipulation.txt' },
+  { name: 'M2P1 generating frequency tables', path: 'M2P1 generating frequency tables.txt' },
+  { name: 'M2P3 performing one-sample + -tests', path: 'M2P3 performing one-sample + -tests.txt' },
+  { name: 'M2P5 performing paired t tests', path: 'M2P5 performing paired t tests.txt' },
+  { name: 'M2P6 performing one-way ANOVA', path: 'M2P6 performing one-way ANOVA.txt' }
 
 
     // Add more file paths as needed
